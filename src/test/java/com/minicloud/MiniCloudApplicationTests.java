61493f5c6f496d0e142b0aeebf24cbfd97bfb23b
Roberto de Frutos Jiménez <robertodfj93@gmail.com>
2025-11-04 12:47:50 +0100

    001 Añadimos el proyecto principal creado con spring inicialzr

---
+++ src/test/java/com/minicloud/MiniCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minicloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

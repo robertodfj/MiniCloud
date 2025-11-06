0ddea995d8d9de8e76a7896b01baf360dea41ff0
Roberto de Frutos Jiménez <robertodfj93@gmail.com>
2025-11-06 12:21:59 +0100

    010 Creamos el UserDTO para no exponer directamente la informacion

---
+++ src/main/java/com/minicloud/dto/UserDTO.java
@@ -0,0 +1,30 @@
+package com.minicloud.dto;
+
+public class UserDTO {
+    private String userName;
+    private String email;
+
+    public UserDTO(String userName, String email) {
+        this.userName = userName;
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+
+}

de4b40b40e6f44e24e3a2f998d67de255a203ccf
Roberto de Frutos Jiménez <robertodfj93@gmail.com>
2025-11-10 19:16:15 +0100

    015 Creamos el userService

---
+++ src/main/java/com/minicloud/service/UserService.java
@@ -0,0 +1,36 @@
+package com.minicloud.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.minicloud.model.User;
+import com.minicloud.repository.UserRepository;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private static UserRepository userRepository;
+
+    public static User createUser(User user) {
+        String email = user.getEmail();
+        if (userRepository.existsByEmail(email)) {
+            throw new IllegalArgumentException("Email already in use: " + email);
+            
+        }
+        return userRepository.save(user);
+    }
+
+    public static User getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public static boolean userExistsByUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    public static boolean userExistsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+    
+}

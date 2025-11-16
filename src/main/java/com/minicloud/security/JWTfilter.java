d527212dbe3a0ff36d06c5aba8aa76d6aff3a2fc
Roberto de Frutos Jiménez <robertodfj93@gmail.com>
2025-11-16 14:27:32 +0100

    038 Editamos JWTFilter para evitar eñ aviso de errores al cambiar un import

--- src/main/java/com/minicloud/security/JWTfilter.java
+++ src/main/java/com/minicloud/security/JWTfilter.java
@@ -3,6 +3,7 @@ package com.minicloud.security;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,10 +26,11 @@ public class JWTfilter extends OncePerRequestFilter {
     private CustomUserDetailsService userDetailsService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain)
-                                    throws ServletException, IOException {
+    protected void doFilterInternal(
+        @NonNull HttpServletRequest request,
+        @NonNull HttpServletResponse response,
+        @NonNull FilterChain filterChain)
+        throws ServletException, IOException {
 
         String header = request.getHeader("Authorization");
         String token = null;
